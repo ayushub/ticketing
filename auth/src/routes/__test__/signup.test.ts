06bb1e5dd188c3264f67c92475e0317085f14ddb
Ayush Agrawal <aagrawal@bunnings.com.au>
2022-04-16 21:37:11 +1000

    test duplicate email checking

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -57,3 +57,21 @@ it("returns a 400 with missing email and password", async () => {
     })
     .expect(400);
 });
+
+it("disallows duplicate email", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.cpomo",
+      password: "password",
+    })
+    .expect(201);
+
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.cpomo",
+      password: "password",
+    })
+    .expect(400);
+});
