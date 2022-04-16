a8e939a8c1d84e3fcd1d73bd35b6c41565b612ef
Ayush Agrawal <aagrawal@bunnings.com.au>
2022-04-17 08:02:52 +1000

    updated server side rendering to get init data

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,25 @@
-const LandingPage = () => {
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+  // axios.get("/api/users/currentuser");
+
   return <h1>Landing Page</h1>;
+};
+
+LandingPage.getInitialProps = async () => {
+  let baseurl = "";
+  if (typeof window === "undefined") {
+    // server side call
+    baseurl = "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local";
+  }
+  const { data } = await axios.get(`${baseurl}/api/users/currentuser`, {
+    headers: {
+      Host: "ticketing.dev", // this is required for the server requests when domain is that baseurl
+    },
+  });
+
+  return data;
 };
 
 export default LandingPage;
