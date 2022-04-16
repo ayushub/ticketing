46c2e3fa5e0dca046f61c3720f741186aae3f8c0
Ayush Agrawal <aagrawal@bunnings.com.au>
2022-04-17 08:07:23 +1000

    fixed missing headers from incoming request

--- client/pages/index.js
+++ client/pages/index.js
@@ -7,16 +7,15 @@ const LandingPage = ({ currentUser }) => {
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async () => {
+LandingPage.getInitialProps = async ({ req }) => {
   let baseurl = "";
   if (typeof window === "undefined") {
     // server side call
     baseurl = "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local";
   }
   const { data } = await axios.get(`${baseurl}/api/users/currentuser`, {
-    headers: {
-      Host: "ticketing.dev", // this is required for the server requests when domain is that baseurl
-    },
+    // mainly for host (when making api call with the above baseurl since nginx doesn't understand that domain) and cookie
+    headers: req.headers,
   });
 
   return data;
