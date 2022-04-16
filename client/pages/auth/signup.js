59b9271fbe3a8b7d3969f019756fae0a36563f4d
Ayush Agrawal <aagrawal@bunnings.com.au>
2022-04-16 23:55:55 +1000

    redirect on sign up

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import axios from "axios";
+import Router from "next/router";
 import useRequest from "../../hooks/useRequest";
 
 export default () => {
@@ -12,9 +12,11 @@ export default () => {
       email,
       password,
     },
+    onSuccess: () => Router.push("/"),
   });
   const onSubmit = async (event) => {
     event.preventDefault();
+
     doRequest();
   };
 
