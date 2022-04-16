0f35bc0d6864c2ec0122f24366fc2757eb218222
Ayush Agrawal <aagrawal@bunnings.com.au>
2022-04-17 08:53:59 +1000

    get init props within app component

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,28 @@
 import "bootstrap/dist/css/bootstrap.css";
+import buildClient from "../api/build-client";
 
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps }) => {
+  console.log("props", pageProps);
+  return (
+    <div>
+      <h1>Header</h1>
+      <Component {...pageProps} />;
+    </div>
+  );
 };
+
+AppComponent.getInitialProps = async (appContext) => {
+  const { data } = await buildClient(appContext.ctx).get(
+    "/api/users/currentuser"
+  );
+  let pageProps = {};
+  if (appContext.Component.getInitialProps)
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  console.log("from ", pageProps);
+  return {
+    pageProps,
+    ...data,
+  };
+};
+
+export default AppComponent;
