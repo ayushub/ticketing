6bb118c0e1444d894145d844e3dfbda5335650fe
Ayush Agrawal <aagrawal@bunnings.com.au>
2022-04-16 20:11:34 +1000

    current user check extracted as a middleware

---
+++ auth/src/middlewares/current-user.ts
@@ -0,0 +1,36 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+interface UserPayload {
+  id: string;
+  email: string;
+}
+
+// adding in currentUser property to Request obj of type UserPayload
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: UserPayload;
+    }
+  }
+}
+
+export const currentUser = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (!req.session?.jwt) {
+    return next();
+  }
+
+  try {
+    const payload = jwt.verify(
+      req.session.jwt,
+      process.env.JWT!
+    ) as UserPayload;
+
+    req.currentUser = payload;
+  } catch (err) {}
+  return next();
+};
