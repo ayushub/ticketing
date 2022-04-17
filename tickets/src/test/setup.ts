94d36d225c3ef5fc5c4d9e02754339f0c1e1573a
Ayush Agrawal <aagrawal@bunnings.com.au>
2022-04-17 11:30:07 +1000

    added signin to ticketing tests

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -2,9 +2,10 @@ import request from "supertest";
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
 import { app } from "../app";
+import jwt from "jsonwebtoken";
 
 declare global {
-  var signin: () => Promise<string[]>;
+  var signin: () => string[];
 }
 
 let mongo: any;
@@ -29,18 +30,22 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-// global.signin = async () => {
-//   const email = "test@test.com";
-//   const password = "password";
+global.signin = () => {
+  const email = "test@test.com";
+  const id = "some-random-id";
+  const payload = { id, email };
 
-//   const response = await request(app)
-//     .post("/api/users/signup")
-//     .send({
-//       email,
-//       password,
-//     })
-//     .expect(201);
+  //create jwt out of the payload
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
 
-//   const cookie = response.get("Set-Cookie");
-//   return cookie;
-// };
+  //build session ob { jwt: MY_JWT }
+  const session = { jwt: token };
+
+  // turn that session into JSON
+  const sessionJSON = JSON.stringify(session);
+
+  // take json and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+
+  return [`session=${base64}`];
+};
