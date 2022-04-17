c2ba2797c61937e2dd68b2de141c64896b39ef67
Ayush Agrawal <aagrawal@bunnings.com.au>
2022-04-17 11:53:43 +1000

    added ticket model

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,3 +1,4 @@
+import { RequestValidationError } from "@aatix/common";
 import request from "supertest";
 import { app } from "../../app";
 
@@ -17,10 +18,41 @@ it("returns a status other than 401 if signed in", async () => {
     .send({});
 
   expect(response.status).not.toEqual(401);
+  // add in a check to see if it was the request validation error
 });
 
-it("returns an error if an invalid title is provided", async () => {});
+it("returns an error if an invalid title is provided", async () => {
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title: "",
+      price: 10,
+    });
 
-it("returns an error if an invalid title is provided", async () => {});
+  expect(response.status).toEqual(400);
+});
 
-it("create a ticket with valid inputs", async () => {});
+it("returns an error if an invalid price is provided", async () => {
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title: "test",
+      price: -1,
+    });
+
+  expect(response.status).toEqual(400);
+});
+
+it("create a ticket with valid inputs", async () => {
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title: "test",
+      price: 10,
+    });
+
+  expect(response.status).toEqual(201);
+});
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,10 +1,22 @@
-import { requireAuth } from "@aatix/common";
+import { requireAuth, validateRequest } from "@aatix/common";
 import express, { Request, Response } from "express";
+import { body } from "express-validator";
 
 const router = express.Router();
 
-router.post("/api/tickets", requireAuth, (req: Request, res: Response) => {
-  res.sendStatus(200);
-});
+router.post(
+  "/api/tickets",
+  requireAuth,
+  [
+    body("title").notEmpty().withMessage("Title must be provided"),
+    body("price")
+      .isFloat({ gt: 0 })
+      .withMessage("Price must not be greater than 0"),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {
+    res.sendStatus(200);
+  }
+);
 
 export { router as createTicketRouter };
