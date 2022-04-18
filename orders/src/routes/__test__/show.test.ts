39937d1c23ad4a899d50746e35254d400886b1ab
Ayush Agrawal <aagrawal@bunnings.com.au>
2022-04-19 08:36:28 +1000

    added show service

--- orders/src/routes/__test__/show.test.ts
+++ orders/src/routes/__test__/show.test.ts
@@ -1,7 +1,75 @@
+import mongoose from "mongoose";
 import request from "supertest";
 import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
 
-it("has a route handler listening to /api/orders for get requests", async () => {
-  const response = await request(app).get("/api/orders").send();
+it("has a route handler listening to /api/orders/:orderId for get requests", async () => {
+  const response = await request(app).get("/api/orders/123").send();
   expect(response.status).not.toEqual(404);
+});
+
+it("throws bad request on invalid order id", async () => {
+  const response = await request(app)
+    .get(`/api/orders/1234asdf`)
+    .set("Cookie", global.signin())
+    .send()
+    .expect(400);
+});
+
+it("throws not found on invalid order", async () => {
+  const response = await request(app)
+    .get(`/api/orders/${new mongoose.Types.ObjectId()}`)
+    .set("Cookie", global.signin())
+    .send()
+    .expect(404);
+});
+
+it("throws not auth error if user does not own the order", async () => {
+  // create a ticket
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 21,
+  });
+  await ticket.save();
+
+  const user = global.signin();
+  // make a request
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // make a request to fetch the order
+  await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set("Cookie", global.signin())
+    .send()
+    .expect(401);
+});
+
+it("fetches the order", async () => {
+  // create a ticket
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 21,
+  });
+  await ticket.save();
+
+  const user = global.signin();
+  // make a request
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // make a request to fetch the order
+  const response = await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set("Cookie", user)
+    .send()
+    .expect(200);
+
+  expect(response.body.id).toEqual(order.id);
 });
--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,9 +1,28 @@
+import {
+  BadRequestError,
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+} from "@aatix/common";
 import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
-router.get("/api/orders/:orderId", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.get(
+  "/api/orders/:orderId",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    // TODO: add a check for the req.params.orderId
+    if (!mongoose.Types.ObjectId.isValid(req.params.orderId))
+      throw new BadRequestError("Invalid order id");
+    const order = await Order.findById(req.params.orderId).populate("ticket");
+    if (!order) throw new NotFoundError();
+
+    if (order.userId !== req.currentUser!.id) throw new NotAuthorizedError();
+    res.send(order);
+  }
+);
 
 export { router as showOrderRouter };
