96e9bcb427258e92417354f4b5eefdd26b64efa6
Ayush Agrawal <aagrawal@bunnings.com.au>
2022-04-18 20:22:41 +1000

    create order service added

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -5,3 +5,14 @@ it("has a route handler listening to /api/orders for post requests", async () =>
   const response = await request(app).post("/api/orders").send({});
   expect(response.status).not.toEqual(404);
 });
+
+it("throws error on invalid ticket", async () => {
+  const cookie = global.signin();
+  const response = await request(app)
+    .post("/api/orders")
+    .set("Cookie", cookie)
+    .send({
+      ticketId: "1234",
+    });
+  expect(response.status).not.toEqual(404);
+});
--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,18 @@
-import { requireAuth, validateRequest } from "@aatix/common";
+import {
+  BadRequestError,
+  NotFoundError,
+  requireAuth,
+  validateRequest,
+} from "@aatix/common";
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
 import mongoose from "mongoose";
+import { Ticket } from "../models/ticket";
+import { OrderStatus, Order } from "../models/order";
 
 const router = express.Router();
+
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
 
 router.post(
   "/api/orders",
@@ -17,7 +26,30 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
-    res.send({});
+    const { ticketId } = req.body;
+
+    // find the ticket that user is trying to order iun the db
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) throw new NotFoundError();
+
+    // make sure that this ticket isnt already reserved
+    const isReserved = await ticket.isReserved();
+    if (isReserved) throw new BadRequestError("Ticket is already reserved");
+
+    // calc an expiration date of the order
+    const expiresAt = new Date();
+    expiresAt.setSeconds(expiresAt.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+
+    // build the order and save to db
+    const order = await Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt,
+      ticket,
+    });
+
+    // publish an event for the created order
+    res.status(201).send(order);
   }
 );
 
