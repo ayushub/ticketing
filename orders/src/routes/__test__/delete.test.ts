2a380c01bbef7f079c0d780d59fd93dd377ac1b6
Ayush Agrawal <aagrawal@bunnings.com.au>
2022-04-19 08:49:42 +1000

    added delete api

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -1,7 +1,81 @@
+import { OrderStatus } from "@aatix/common";
+import mongoose from "mongoose";
 import request from "supertest";
 import { app } from "../../app";
+import { Order } from "../../models/order";
+import { Ticket } from "../../models/ticket";
 
 it("has a route handler listening to /api/orders for delete requests", async () => {
   const response = await request(app).delete("/api/orders/erwq").send();
   expect(response.status).not.toEqual(404);
 });
+
+it("throws bad request on invalid order id", async () => {
+  const response = await request(app)
+    .delete(`/api/orders/1234asdf`)
+    .set("Cookie", global.signin())
+    .send()
+    .expect(400);
+});
+
+it("throws not found on invalid order", async () => {
+  const response = await request(app)
+    .delete(`/api/orders/${new mongoose.Types.ObjectId()}`)
+    .set("Cookie", global.signin())
+    .send()
+    .expect(404);
+});
+
+it("throws not auth error if user does not own the order", async () => {
+  // create a ticket
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 21,
+  });
+  await ticket.save();
+
+  const user = global.signin();
+  // make a request
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // make a request to delete the order
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set("Cookie", global.signin())
+    .send()
+    .expect(401);
+});
+
+it("marks order as cancelled", async () => {
+  // create a ticket
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 21,
+  });
+  await ticket.save();
+
+  const user = global.signin();
+  // make a request to create an order
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // make a request to cancel the order
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set("Cookie", user)
+    .send()
+    .expect(204);
+
+  const cancelledOrder = await Order.findById(order.id);
+
+  expect(cancelledOrder?.status).toEqual(OrderStatus.Cancelled);
+});
+
+it.todo("emits a order cancelled event");
