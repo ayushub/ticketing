65a5f48dfad530cde494e1937d50bd433df21691
Ayush Agrawal <aagrawal@bunnings.com.au>
2022-04-21 06:45:16 +1000

    install deps

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -1,16 +1,6 @@
-import { app } from "./app";
-import mongoose from "mongoose";
 import { natsWrapper } from "./nats-wrapper";
-import { OrderCreatedListener } from "./events/listeners/order-created";
-import { OrderCancelledListener } from "./events/listeners/order-cancelled";
 
 const start = async () => {
-  if (!process.env.JWT_KEY) {
-    throw new Error("JWT_KEY must be defined");
-  }
-  if (!process.env.MONGO_URI) {
-    throw new Error("MONGO_URI must be defined");
-  }
   if (!process.env.NATS_URL) {
     throw new Error("NATS_URL must be defined");
   }
@@ -33,19 +23,9 @@ const start = async () => {
     });
     process.on("SIGINT", () => natsWrapper.client.close());
     process.on("SIGTERM", () => natsWrapper.client.close());
-
-    new OrderCreatedListener(natsWrapper.client).listen();
-    new OrderCancelledListener(natsWrapper.client).listen();
-
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("connected to mongo db");
   } catch (err) {
     console.error(err);
   }
-
-  app.listen(3000, () => {
-    console.log("Listening on port 3000!");
-  });
 };
 
 start();
