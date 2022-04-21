2aa2fa085cbf34aaeb95c89cb2920c27f6bd7c33
Ayush Agrawal <aagrawal@bunnings.com.au>
2022-04-21 20:10:29 +1000

    added order created listener

---
+++ payments/src/events/listeners/order-created.ts
@@ -0,0 +1,36 @@
+import {
+  Listener,
+  OrderCreatedEvent,
+  OrderStatus,
+  Subjects,
+} from "@aatix/common";
+import { Message } from "node-nats-streaming";
+import { Order } from "../../models/order";
+import { queueGroupName } from "./queue-group-name";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  subject: Subjects.OrderCreated = Subjects.OrderCreated;
+  queueGroupName: string = queueGroupName;
+  async onMessage(
+    data: {
+      id: string;
+      status: OrderStatus;
+      userId: string;
+      expiresAt: string;
+      version: number;
+      ticket: { id: string; price: number };
+    },
+    msg: Message
+  ): Promise<void> {
+    const order = Order.build({
+      id: data.id,
+      price: data.ticket.price,
+      status: data.status,
+      userId: data.userId,
+      version: data.version,
+    });
+    await order.save();
+
+    msg.ack();
+  }
+}
