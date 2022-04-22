8d6d28b38c524da69a427092ed85c6ead21e1b8c
Ayush Agrawal <aagrawal@bunnings.com.au>
2022-04-23 07:48:45 +1000

    added stripe checkout

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
+import StripeCheckout from "react-stripe-checkout";
 
-const OrderShow = ({ order }) => {
+const OrderShow = ({ order, user }) => {
   const [timeLeft, setTimeLeft] = useState(0);
 
   useEffect(() => {
@@ -20,14 +21,24 @@ const OrderShow = ({ order }) => {
     return <div>Order Expired</div>;
   }
 
-  return <div>Time left to pay: {timeLeft} seconds</div>;
+  return (
+    <div>
+      Time left to pay: {timeLeft} seconds
+      <StripeCheckout
+        token={(token) => console.log(token)}
+        stripeKey="pk_test_51KrKCsHJHm0KTh4WLcF35wknertiFIRW70RxGCMIrNAv7XPwJhRseAcQDpOhHpbTVGGjnUDde0XA4R6izgTNDJx900AxvYW9fK"
+        amount={order.ticket.price * 100}
+        email={user.email}
+      />
+    </div>
+  );
 };
 
-OrderShow.getInitialProps = async (context, client) => {
+OrderShow.getInitialProps = async (context, client, user) => {
   const { orderId } = context.query;
   const { data } = await client.get(`/api/orders/${orderId}`);
 
-  return { order: data };
+  return { order: data, user };
 };
 
 export default OrderShow;
