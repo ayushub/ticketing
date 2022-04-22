8e34d055f8d0514a116faa620989471d55c3d6bc
Ayush Agrawal <aagrawal@bunnings.com.au>
2022-04-23 06:44:42 +1000

    redirect on submit

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -15,9 +15,8 @@ const AppComponent = ({ Component, pageProps, currentUser }) => {
 };
 
 AppComponent.getInitialProps = async (appContext) => {
-  const { data } = await buildClient(appContext.ctx).get(
-    "/api/users/currentuser"
-  );
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get("/api/users/currentuser");
   let pageProps = {};
   if (appContext.Component.getInitialProps)
     pageProps = await appContext.Component.getInitialProps(
