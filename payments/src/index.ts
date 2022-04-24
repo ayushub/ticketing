e48857ed56a3440f057ade5acb200fc97653c771
ayushub <agrawal.ayush89@gmail.com>
2022-04-24 19:06:12 +1000

    deploy (#7)

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -5,6 +5,7 @@ import { OrderCreatedListener } from "./events/listeners/order-created";
 import { OrderCancelledListener } from "./events/listeners/order-cancelled";
 
 const start = async () => {
+  console.log("Starting...");
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
